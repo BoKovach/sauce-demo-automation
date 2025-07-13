61a106b6f797629e5fcead9e7bc1ed2988db3239
Bo Kovach <bo.kovachh@gmail.com>
2025-07-13 19:06:15 +0300

    Add json loader file

---
+++ utils/data_loader.py
@@ -0,0 +1,8 @@
+import json
+import os
+
+
+def load_login_data():
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'tests', 'login_logout_data.json')
+    with open(file_path, 'r') as file:
+        return json.load(file)
