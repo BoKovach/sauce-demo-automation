975a95900407b2233cf708c61e63a8e731fa127c
Bo Kovach <bo.kovachh@gmail.com>
2025-07-13 17:54:34 +0300

    Added page object login_page

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_input = (By.ID, 'user-name')
+        self.password_input = (By.ID, 'password')
+        self.login_button = (By.ID, 'login-button')
+        self.login_error_message = (By.CSS_SELECTOR,
+                                  '#login_button_container > div > form > div.error-message-container.error > h3')
+
+    def open(self):
+        self.driver.get('https://www.saucedemo.com/')
+
+    def login(self, username, password):
+        self.driver.find_element(*self.username_input).send_keys(username)
+        self.driver.find_element(*self.password_input).send_keys(password)
+        self.driver.find_element(*self.login_button).click()
+
+    def error_message(self):
+        return self.driver.find_element(*self.login_error_message).text
